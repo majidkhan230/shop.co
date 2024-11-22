5c0d6c9bdf4702a5b99b0cbe40b582f098874046
MajidKh230 <khanmajid230@gmail.com>
2024-11-22 18:19:16 +0500

    add some work

---
+++ src/components/CustomCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card, Rate } from 'antd';
+const { Meta } = Card;
+const CustomCard = ({data}) => {
+    const {title,image,price,description,rating} = data
+return(
+    <Card
+    hoverable
+    style={{
+      width: "95%",
+     
+    }}
+    cover={<img alt="example" src={image} style={{
+        objectFit:"contain",
+        width:'100%',
+        height:"200px",
+        marginTop:"10px",
+    }} />}
+  >
+    <Meta title={title} />
+    <div className="flex md:space-x-2 mt-2  ">
+    <Rate allowHalf defaultValue={rating.rate} className='flex flex-nowrap text-sm ' />
+<span>{rating.rate}/5</span>
+    </div>
+    <h1 className='text-xl md:text-2xl font-bold my-2'>${price}</h1>
+  </Card>
+)
+};
+export default CustomCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Button from "../components/Button";
 import Counter from "../components/Counter";
+import Banner from "../components/Banner";
 
 function Home() {
   return (
-    <div className="px-4 md:px-20 w-full bg-[#F2F0F1]   flex flex-col  md:flex-row">
+<div>
+  {/* Top Page */}
+<div>
+<div className="px-4 md:px-20 w-full bg-[#F2F0F1]   flex flex-col  md:flex-row">
       <div className="left md:w-1/2 flex flex-col  justify-center ">
         <h1 className="py-5 text-4xl font-[integralcf] text-start font-semibold  md:text-5xl  md:mb-10">
           FIND CLOTHES <br /> THAT MATCHES <br /> YOUR STYLE
@@ -32,7 +36,25 @@ function Home() {
       <div className="right md:w-1/2 ">
         <img src="../assets/images/trendy-couple.jpg" className="object-cover object-top w-full h-[600px]" alt="" />
       </div>
+      
     </div>
+    <div className="black bg-black text-white h-30  md:h-20 p-2   flex justify-evenly  flex-wrap">
+      <img src="/assets/images/p1.png" className="object-contain "></img>
+      <img src="/assets/images/p2.png" className="object-contain"></img>
+      <img src="/assets/images/p3.png" className="object-contain"></img>
+      <img src="/assets/images/p4.png" className="object-contain"></img>
+      <img src="/assets/images/p5.png" className="object-contain"></img>
+    </div>
+</div>
+
+{/* page2 */}
+<Banner title={"New Arrivals"} />
+
+
+</div>
+
+
+
   );
 }
 
